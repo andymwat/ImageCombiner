f4cf9db28a01328f1ebbb6527b39502467771001
Andrew Watson <andrew.watson@ufl.edu>
2020-04-30 01:08:30 -0400

    Add project files.

---
+++ ImageCombiner/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace ImageCombiner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button7 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button7);
+            this.splitContainer1.Panel2.Controls.Add(this.button6);
+            this.splitContainer1.Panel2.Controls.Add(this.button5);
+            this.splitContainer1.Panel2.Controls.Add(this.button4);
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Panel2.Controls.Add(this.statusStrip1);
+            this.splitContainer1.Panel2.Controls.Add(this.checkBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.button3);
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(1221, 680);
+            this.splitContainer1.SplitterDistance = 506;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(506, 680);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(415, 609);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(102, 21);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Use Border";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(3, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Choose Color";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(524, 71);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(175, 125);
+            this.panel1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(589, 482);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 77);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "📂Open";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(415, 582);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(140, 21);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Automatic update";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 655);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(711, 25);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(151, 20);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // listView1
+            // 
+            this.listView1.AllowDrop = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(3, 12);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(406, 640);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(415, 45);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 49);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "🗑";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(415, 149);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(87, 47);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "↓";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(415, 100);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(87, 43);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "↑";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(111, 30);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 43);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "\"Image Files(*.bmp;*.jpg;*.png)|*.bmp;*.jpg;*.png;\"";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(589, 565);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(110, 87);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "💾Save";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "\"Image Files(*.bmp;*.jpg;*.png)|*.bmp;*.jpg;*.png;\"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1221, 680);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Image Combiner";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ImageCombiner/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageCombiner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            button2.Enabled = checkBox1.Checked;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            refreshPreview();
+        }
+
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+
+        private enum ResizeMode
+        {
+            ShrinkLarger,
+            ExpandSmaller,
+            NoResize_Transparency,
+            NoResize_BorderColorBackground
+        }
+
+        /// <summary>
+        /// Combines two images above each other, with an optional border.
+        /// </summary>
+        /// <param name="top">The top image.</param>
+        /// <param name="bottom">The bottom image.</param>
+        /// <param name="borderSize">The size of the border, in pixels. -1 is no border.</param>
+        /// <param name="borderColor">The color of the border, if there is one.</param>
+        /// <param name="resizeMode">Options for resizing smaller/larger images.</param>
+        /// <returns></returns>
+        private Image combineImages(Image top, Image bottom, int borderSize, Color borderColor, ResizeMode resizeMode)
+        {
+            int whichSmaller = 0; //-1 = bottom, 0 = even; 1 = top
+            if (top.Width < bottom.Width)
+                whichSmaller = 1;
+            if (bottom.Width < top.Width)
+                whichSmaller = -1;
+
+            Image resized = new Bitmap(1,1);
+            if (whichSmaller == 0)
+            {
+                resized = new Bitmap(top.Width, 1);//set width for later (border calculation)
+            }
+            int totalHeight, totalWidth;
+            totalHeight = totalWidth = 0;
+            totalWidth = top.Width;
+            if (whichSmaller != 0 && resizeMode == ResizeMode.ExpandSmaller) //if needs to be resized and expanding the smaller one
+            {
+                
+                double scaleRatio; //what the smaller image needs to be multiplied by to scale up
+                if (whichSmaller == -1) //bottom smaller
+                {
+                    scaleRatio = (double)top.Width / bottom.Width;
+                    resized = ResizeImage(bottom, top.Width, Convert.ToInt32(bottom.Height * scaleRatio));
+                    totalHeight = resized.Height + top.Height;
+                }
+                else if (whichSmaller == 1) //top smaller
+                {
+                    scaleRatio = (double)bottom.Width / top.Width;
+                    resized = ResizeImage(top, bottom.Width, Convert.ToInt32(top.Height * scaleRatio));
+                    totalHeight = resized.Height + bottom.Height;
+                }
+                else
+                {
+                    throw new Exception("Should never get here.");
+                }
+
+                totalWidth = resized.Width;
+            }
+
+            
+            if (borderSize > 0)
+            {
+                totalHeight += borderSize;
+            }
+
+
+            Image resultImage = new Bitmap(totalWidth, totalHeight);
+            
+            Graphics g = Graphics.FromImage(resultImage);
+            Point offset = new Point(0, 0);
+
+
+            //draw top image
+            if (whichSmaller != 0 && resizeMode == ResizeMode.ExpandSmaller)
+            {
+                if (whichSmaller == -1) //bottom smaller
+                {
+                    g.DrawImage(top, offset.X, offset.Y, top.Width, top.Height);
+                    offset.Y += top.Height;
+                }
+                else if (whichSmaller == 1) //top smaller
+                {
+                    g.DrawImage(resized, offset.X, offset.Y, resized.Width, resized.Height);
+                    offset.Y += resized.Height;
+                }
+                else
+                {
+                    throw new Exception("Should never get here.");
+                }
+                
+            }
+
+            //draw border (if any)
+            if (borderSize > 0)
+            {
+                g.FillRectangle(new SolidBrush(borderColor), offset.X, offset.Y, resized.Width, borderSize);
+                offset.Y += borderSize;
+            }
+
+
+            //draw bottom image
+            if (whichSmaller != 0 && resizeMode == ResizeMode.ExpandSmaller)
+            {
+                if (whichSmaller == -1) //bottom smaller
+                {
+                    g.DrawImage(resized, offset.X, offset.Y, resized.Width, resized.Height);
+                }
+                else if (whichSmaller == 1) //top smaller
+                {
+                    g.DrawImage(bottom, offset.X, offset.Y, bottom.Width, bottom.Height);
+                }
+                else
+                {
+                    throw new Exception("Should never get here.");
+                }
+
+            }
+            
+
+            return resultImage;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = !checkBox2.Checked;
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private Color color = Color.Black;
+        private List<Image> images = new List<Image>();
+        private List<string> imageNames = new List<string>();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                color = colorDialog1.Color;
+                pictureBox2.Image = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+                Graphics g = Graphics.FromImage(pictureBox2.Image);
+                g.Clear(color);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                foreach (string file in openFileDialog1.FileNames)
+                {
+                    Image newImage = Image.FromFile(file);
+                    images.Add(newImage);
+                    imageNames.Add(Path.GetFileName(file));
+                }
+                toolStripStatusLabel1.Text = "File(s) loaded.";
+
+            }
+
+            if (checkBox2.Checked)
+            {
+                refreshPreview();
+            }
+
+            listView1.Items.Clear();
+            foreach (string i in imageNames)
+            {
+                listView1.Items.Add(i);
+            }
+            
+
+        }
+
+        private void refreshPreview()
+        {
+            if (images.Count < 2)
+            {
+                return;
+            }
+            Image final = images[0];
+            for (int i = 1; i < images.Count; i++)
+            {
+                final = combineImages(final, images[i], 10, color, ResizeMode.ExpandSmaller);
+            }
+
+            pictureBox1.Image = final;
+        }
+
+
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            DialogResult result = saveFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+                toolStripStatusLabel1.Text = "Saved.";
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "Save canceled.";
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                color = colorDialog1.Color;
+                pictureBox2.Image = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+                Graphics g = Graphics.FromImage(pictureBox2.Image);
+                g.Clear(color);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count <= 0 || listView1.FocusedItem == null)
+                return;
+
+            images.RemoveAt(listView1.FocusedItem.Index);
+            imageNames.RemoveAt(listView1.FocusedItem.Index);
+            listView1.Items.Clear();
+            foreach (string i in imageNames)
+            {
+                listView1.Items.Add(i);
+            }
+        }
+    }
+}
